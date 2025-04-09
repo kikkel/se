7ae5fd0b30230ee53f552f0911f63a5858f16aa2
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-04-09 14:49:14 +0200

    copilot hat uns eine spec erstellt :)

--- starrealms/src/test/scala/example/de/htwg/se/model/PlayingFieldSpec.scala
+++ starrealms/src/test/scala/example/de/htwg/se/model/PlayingFieldSpec.scala
@@ -17,22 +17,88 @@ class PlayingFieldSpec extends AnyWordSpec with Matchers {
         playingField.columns should equal (3 * playingField.pfUnit)
 
     }
-    "contain 8 card slots in the top third" in {
-        val playingField = new PlayingField
-        playingField.numRecs shouldEqual 8
-        
-    }
 
     "calculate correct card width and height with 3:2 ratio" in {
         val playingField = new PlayingField
         playingField.rectHeight.toFloat / playingField.rectWidth.toFloat shouldBe  1.5f +- 0.01f
     }
-    "center the cards in the top third vertically" in {
+
+    "contain 8 cards in the top third" in {
+        val playingField = new PlayingField
+        playingField.numRecs should equal (8)
+    }
+
+    "contain 7 gaps between the cards" in {
+        val playingField = new PlayingField
+        playingField.numGaps should equal (playingField.numRecs - 1)
+    }
+
+    "contain half a gap on either end of the row" in {
+        val playingField = new PlayingField
+        playingField.sideGapwidth should equal (0)
+    }
+
+    "calculate the total gap width" in {
+        val playingField = new PlayingField
+        playingField.totalGapWidth should equal (playingField.columns - (playingField.numRecs * playingField.rectWidth))
+    }
+
+    "calculate the card row width" in {
+        val playingField = new PlayingField
+        playingField.cardRowWidth should equal (playingField.numRecs * playingField.rectWidth + playingField.numGaps * playingField.gapWidth)
+    }
+
+    "calculate the gap width" in {
+        val playingField = new PlayingField
+        playingField.gapWidth should equal (playingField.columns / 25)
+    }
+
+    "calculate the card width" in {
+        val playingField = new PlayingField
+        playingField.rectWidth should equal (playingField.gapWidth * 2)
+    }
+
+    "calculate the card height" in {
+        val playingField = new PlayingField
+        playingField.rectHeight should equal (Math.min((playingField.gapWidth * 3), (playingField.rows / 5)))
+    }
+
+    "calculate the gap height" in {
+        val playingField = new PlayingField
+        playingField.gapHeight should equal (playingField.rows / 5)
+    }
+
+    "calculate the top third of the playing field" in {
+        val playingField = new PlayingField
+        playingField.topThird should equal (playingField.rows / 3)
+    }
+
+    "calculate the centered row" in {
+        val playingField = new PlayingField
+        playingField.centeredRow should equal (Math.max((playingField.topThird - playingField.rectHeight) / 2, 0))
+    }
+
+
+
+   /* "center the cards in the top third vertically" in {
         val playingField = new PlayingField
         val expectedTopThird = playingField.rows / 3
         val spaceAboveCards = playingField.centeredRow
         val spaceBelowCards = playingField.rows - (playingField.centeredRow + playingField.rectHeight)
+    } */
+
+    "draw the playing field with cards" in {
+        val playingField = new PlayingField
+        playingField.drawField() // This will print the playing field to the console
     }
+
+    "draw the playing field with empty spaces" in {
+        val playingField = new PlayingField
+        playingField.drawField() // This will print the playing field to the console
+    }
+
+   
+
   }
 }
 
