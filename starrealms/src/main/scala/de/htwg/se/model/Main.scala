72046c400a31c5277b9154a1474c714007799742
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-04-11 11:44:53 +0200

    GROßE REALISIERUNG!! Playingfield schreiben wir neu, das andere heißt jetzt SETUP

--- starrealms/src/main/scala/de/htwg/se/model/Main.scala
+++ starrealms/src/main/scala/de/htwg/se/model/Main.scala
@@ -2,7 +2,7 @@ package de.htwg.se.starrealms.model
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val playingField = new PlayingField(20) // Adjust the dimension as needed
+    val playingField = new PlayingField() 
     playingField.drawField()
   }
 }
