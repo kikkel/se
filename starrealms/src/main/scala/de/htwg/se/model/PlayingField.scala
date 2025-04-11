f4e9643af56324a7c48fed26b0b903ede5cc3a3f
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-04-11 11:27:48 +0200

    parameter/konstruktor in main/playingfield.scala eingebaut und columns/rows entfernt => gibt jetzt nur noch dimension!!!
    in main.scala die parameter ergänzt

--- starrealms/src/main/scala/de/htwg/se/model/PlayingField.scala
+++ starrealms/src/main/scala/de/htwg/se/model/PlayingField.scala
@@ -1,34 +1,33 @@
 package de.htwg.se.starrealms.model
-class PlayingField {
-    val pfUnit = 10 //playing field unit
-    val rows = 2 * pfUnit
-    val columns = 3 * pfUnit
-    val borderWidth = columns * 3/2+3 // Top/Bottom border
+
+class PlayingField(val dimension: Int) {
+
+    val borderWidth = dimension * 3/2+3 // Top/Bottom border
 
 
 
-    val gapWidth = (columns / 25).toInt
+    val gapWidth = (dimension / 25).toInt
     val rectWidth = gapWidth * 2 //ratio 2:3
-    val rectHeight = Math.min((gapWidth * 3), (rows / 5)) // Ensure it doesn't exceed top third
+    val rectHeight = Math.min((gapWidth * 3), (dimension / 5)) // Ensure it doesn't exceed top third
 
     val numRecs = 8
     val numGaps = numRecs - 1
-    val totalGapWidth = columns - (numRecs * rectWidth)
+    val totalGapWidth = dimension - (numRecs * rectWidth)
     val cardRowWidth = numRecs * rectWidth + numGaps * gapWidth
     
-    val sideGapwidth = 0 //(columns - cardRowWidth) / 2
-    val gapHeight = rows / 5
+    val sideGapwidth = 0 //(dimension - cardRowWidth) / 2
+    val gapHeight = dimension / 5
 
-    val topThird = rows / 3
+    val topThird = dimension / 3
     val centeredRow = Math.max((topThird - rectHeight) / 2, 0) // Ensure non-negative start
     //--------------------------------------------------------------------------------------------------------------------------------------//
     def drawField(): Unit = {
         val border = "+" + ("-" * borderWidth) + "+" // Top/Bottom border //!!! *3/2+3 ?? verallgemeinern
         println(border)
 
-        for (row <- 0 until rows) {
+        for (row <- 0 until dimension) {
           if (row >= centeredRow && row < centeredRow + rectHeight) {
-              // First few rows contain cards (height of each card)
+              // First few dimension contain cards (height of each card)
               print("|") // Left border
               print((" " * sideGapwidth))
 
