c6025c2d6c98c65b6b02b7f70503e79bc9d6fb7c
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-04-11 12:05:49 +0200

    neuer Test PlayingFieldSpec.scala geschrieben

---
+++ starrealms/src/test/scala/example/de/htwg/se/model/PlayingFieldSpec.scala
@@ -0,0 +1,53 @@
+package example.de.htwg.se.model
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+class PlayingFieldSpec extends AnyWordSpec with Matchers {
+
+  "A PlayingField" should {
+
+    "have a dimension in Parameter" in {
+      val playingField = new PlayingField()
+      val playingFieldUnit = playingField.dimension
+    }
+
+   // "have a scalable width proportion"
+
+   "contain measuring units" in {
+        val playingField = new PlayingField()
+        val unit = playingField.dimension / 8
+   }
+
+   "consist of 3 horizontal partitions" in {
+        val playingField = new PlayingField()
+        val tradeRowHeight = playingField.unit * 2  // 1/4 of the height
+        val player1Height = playingField.unit * 3   // 1/4 + 1/8 (remaining quarter) of the height
+        val player2Height = playingField.unit * 3   // 1/4 + 1/8 (remaining quarter) of the height
+   }
+
+    "contain a trade row" in {
+          val playingField = new PlayingField()
+          val tradeRow = playingField.tradeRow
+    }
+
+    "contain a player1 row" in {
+          val playingField = new PlayingField()
+          val player1Row = playingField.player1Row
+    }
+
+    "contain a player2 row" in {
+          val playingField = new PlayingField()
+          val player2Row = playingField.player2Row
+    }
+
+       "should be adjustable according to player's turn" in {
+        val playingField = new PlayingField()
+        val player1Turn = true
+        val player2Turn = false
+   }
+
+
+  }
+
+}
