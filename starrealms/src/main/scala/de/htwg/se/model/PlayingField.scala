9db87b24fa912f0dc758379027f44d10da9da7be
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-04-23 11:17:04 +0200

    created new branch and started adjusting playingfield.scala

--- starrealms/src/main/scala/de/htwg/se/model/PlayingField.scala
+++ starrealms/src/main/scala/de/htwg/se/model/PlayingField.scala
@@ -5,19 +5,19 @@ class PlayingField(val dimension: Int=30) { // Default dimension is 20
     //val borderWidth = dimension * 3/2+3 // Top/Bottom border
     val unit = dimension / 8
 
-    val tradeSpaceHeight = unit * 2  // 1/4 of the height
+  /*   val tradeRowHeight = unit * 2  // 1/4 of the height
     val player1Height = unit * 3   // 1/4 + 1/8 (remaining quarter) of the height
     val player2Height = unit * 3   // 1/4 + 1/8 (remaining quarter) of the height
-
+ */
     val tradeSpace = (0 until dimension).map(_ => " " * unit)
 
     val player1Row = (0 until dimension).map(_ => " " * unit)
     val borderWidth = player1Row.mkString.length
-    val player2Row = (0 until dimension).map(_ => " " * unit)
+    //val player2Row = (0 until dimension).map(_ => " " * unit)
 
-    val player1Turn = true
+   /*  val player1Turn = true
     val player2Turn = false
-
+ */
     def drawField(): Unit = {
         val border = "+" + ("-" * borderWidth + "+") // Top/Bottom border
         println(border)
@@ -25,8 +25,8 @@ class PlayingField(val dimension: Int=30) { // Default dimension is 20
         for (row <- 0 until dimension) {
             if (row < player1Height) {
                 println("|" + player1Row.mkString + "|") // Player 1 row
-            } else if (row < player1Height + tradeSpaceHeight) {
-                println("|" + tradeSpace.mkString + "|") // Trade row
+            } else if (row < player1Height + tradeRowHeight) {
+                println("|" + tradeRow.mkString + "|") // Trade row
             } else {
                 println("|" + player2Row.mkString + "|") // Player 2 row
             }
