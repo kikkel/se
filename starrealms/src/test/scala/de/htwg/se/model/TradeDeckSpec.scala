9032fcb3be172bfdf10c5fbcacad43812fa04350
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-04-23 11:49:09 +0200

    added starrealms. to all test packages. started trade deck spec

--- starrealms/src/test/scala/de/htwg/se/model/TradeDeckSpec.scala
+++ starrealms/src/test/scala/de/htwg/se/model/TradeDeckSpec.scala
@@ -1,2 +1,9 @@
-package de.htwg.se.model
+package de.htwg.se.starrealms.model
 
+//import de.htwg.se.starrealms.model.TradeDeck
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+//import org.scalatest.BeforeAndAfterEach
+
+
+class 
