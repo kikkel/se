05abd6a48f7b51a328e30aa6d0f2b5283d6d1ab5
koeseazra <azra.koese@gmx.de>
2025-04-23 11:38:51 +0200

    created SetUpSpec.scala

--- starrealms/src/test/scala/de/htwg/se/model/SetUpSpec.scala
+++ starrealms/src/test/scala/de/htwg/se/model/SetUpSpec.scala
@@ -5,7 +5,19 @@ import org.scalatest.matchers.should.Matchers
 
 class SetUpSpec extends AnyWordSpec with Matchers {
 
+    "The SetUp" should {
+        "have a ActionSpace" in {
+        val setUp = new SetUp()
+        val actionSpace = setUp.actionSpace
+        actionSpace should not be null
+        }
 
+        "have a TradeSpace" in {
+        val setUp = new SetUp()
+        val tradeSpace = setUp.tradeSpace
+        tradeSpace should not be null
+        }
+    }
 }
 
 
