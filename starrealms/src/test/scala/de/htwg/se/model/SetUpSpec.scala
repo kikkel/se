1e1ef00b1dde8f4383f661ab7dd0e872445f1c89
koeseazra <azra.koese@gmx.de>
2025-04-23 11:47:30 +0200

    SetUpSpec completed

--- starrealms/src/test/scala/de/htwg/se/model/SetUpSpec.scala
+++ starrealms/src/test/scala/de/htwg/se/model/SetUpSpec.scala
@@ -18,6 +18,50 @@ class SetUpSpec extends AnyWordSpec with Matchers {
         tradeSpace should not be null
         }
     }
+    "The ActionSpace" should {
+        "have a PlayerDeck" in {
+            val setUp = new SetUp()
+            val playerDeck = setUp.actionSpace.playerDeck
+            playerDeck should not be null
+        }
+        "have a PlayerDiscard" in {
+            val setUp = new SetUp()
+            val playerDiscard = setUp.actionSpace.playerDiscard
+            playerDiscard should not be null
+        }
+        "have a Hand" in {
+            val setUp = new SetUp()
+            val hand = setUp.actionSpace.hand
+            hand should not be null
+        }
+        "have a AuthorityNumber" in {
+            val setUp = new SetUp()
+            val authority = setUp.actionSpace.authority
+            authority should not be null
+        }
+    }
+    "The TradeSpace" should {
+        "have a TradeRow" in {
+            val setUp = new SetUp()
+            val tradeRow = setUp.tradeSpace.tradeRow
+            tradeRow should not be null
+        }
+        "have a TradeDeck" in {
+            val setUp = new SetUp()
+            val tradeDeck = setUp.tradeSpace.tradeDeck
+            tradeDeck should not be null
+        }
+        "have a DiscardPile" in {
+            val setUp = new SetUp()
+            val tradeDiscard = setUp.tradeSpace.tradeDiscard
+            tradeDiscard should not be null
+        }
+        "have a ExplorerDeck" in {
+            val setUp = new SetUp()
+            val explorerDeck = setUp.tradeSpace.explorerDeck
+            explorerDeck should not be null
+        }
+    }
 }
 
 
