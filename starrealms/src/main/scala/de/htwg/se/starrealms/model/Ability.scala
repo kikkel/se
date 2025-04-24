3091902a777c042dbfea118b24e963fb975a5ab4
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-04-24 14:40:42 +0200

    untested simplified ability.scala and spec

--- starrealms/src/main/scala/de/htwg/se/starrealms/model/Ability.scala
+++ starrealms/src/main/scala/de/htwg/se/starrealms/model/Ability.scala
@@ -1,3 +1,12 @@
-class Ability {
-  
+package de.htwg.se.starrealms.model
+
+class Ability(val actions: List[String]) {
+  def hasActions: Boolean = {
+    actions.nonEmpty
+  }
+
+  def getActions: List[String] = {
+    actions
+  }
+
 }
