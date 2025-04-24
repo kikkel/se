c298dfd5cbe3b947368a0ce912bb5f703d1b89a5
koeseazra <azra.koese@gmx.de>
2025-04-24 14:08:40 +0200

    created Ability class

--- starrealms/src/main/scala/de/htwg/se/starrealms/model/Player.scala
+++ starrealms/src/main/scala/de/htwg/se/starrealms/model/Player.scala
@@ -1,17 +1,18 @@
 package de.htwg.se.starrealms.model
 
-class Player(
-    val name: String,
-    //val playerDeck: PlayerDeck,
-   /* val actionSpace: ActionSpace,
-    val tradeSpace: TradeSpace,
-    val health: Int = 50 */
-) {
-    /*def this(name: String) = this(name, new PlayerDeck())
-    def this() = this("Player", new PlayerDeck())
+class Player() {
+    var name: String = ""
+    var authority: Int = 50
+    var trade: Int = 8
+    var combat: Int = 2
+    var playerDeck: Int = 10
+    var playerDiscard: Int = 0
+    var playerHand: Int = 5
 
-    override def toString: String = {
-        s"Player($name, $playerDeck)"
-    }*/
+    def this(name: String) {
+        this()
+        this.name = name
+    }
 
+    override def toString: String = s"Player($name, $authority, $trade, $combat, $playerDeck, $playerDiscard, $playerHand)"
 }
--- starrealms/src/test/scala/de/htwg/se/starrealms/model/PlayerSpec.scala
+++ starrealms/src/test/scala/de/htwg/se/starrealms/model/PlayerSpec.scala
@@ -10,7 +10,6 @@ class PlayerSpec extends AnyWordSpec with Matchers {
       val player = new Player("name")
       player.name should be("name")
     }
-/*
     "have a starting authority" in {
       val player = Player("name")
       player.authority should be(50)
@@ -18,12 +17,12 @@ class PlayerSpec extends AnyWordSpec with Matchers {
 
     "have a starting trade" in {
       val player = Player("name")
-      player.trade should be(0)
+      player.trade should be(8)
     }
 
     "have a starting combat" in {
       val player = Player("name")
-      player.combat should be(0)
+      player.combat should be(2)
     }
 
     "have a starting deck size" in {
