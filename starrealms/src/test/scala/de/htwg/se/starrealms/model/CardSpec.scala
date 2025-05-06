a0d757a98bdfdddef00c5681cbbd94fe9a0d3e90
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-05-06 12:48:12 +0200

    Enhances card model unit tests
    Refactors and expands test coverage for card-related classes, including Card, Ship, Base, Faction, CardType, and Ability.
    
    Adds new test cases for properties like abilities, factions, and type-specific behaviors to ensure correctness and completeness.

--- starrealms/src/test/scala/de/htwg/se/starrealms/model/CardSpec.scala
+++ starrealms/src/test/scala/de/htwg/se/starrealms/model/CardSpec.scala
@@ -3,38 +3,201 @@ package de.htwg.se.starrealms.model
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-abstract class CardSpec extends AnyWordSpec with Matchers {
-  "an AbstractCard" should {
-    val ability = new Ability(List("TestAbility"))
-    val cardType = new CardType("TestCardType")
-    val card = new TestCard("TestCard", cardType, ability)
+class CardSpec extends AnyWordSpec with Matchers {
+  "A Card" should {
+    "have a name" in {
+      val card = new Ship("Test Card", new CardType("Ship"))
+      card.getName should be("Test Card")
+    }
+
+    "have a cost" in {
+      val card = new Ship("Test Card", new CardType("Ship"))
+      card.getCardType.getName should be("Ship")
+    }
+
+    "have a faction" in {
+      val card = new Ship("Test Card", new CardType("Ship"), Some(new TradeFederation))
+      card.getFaction should be(Some(new TradeFederation))
+    }
+
+    "have no faction" in {
+      val card = new Ship("Test Card", new CardType("Ship"), None)
+      card.getFaction should be(None)
+    }
+
+    "have a primary ability" in {
+      val ability = new Ability(List("Draw a card"))
+      val card = new Ship("Test Card", new CardType("Ship"), None, Some(ability))
+      card.getPrimaryAbility should be(Some(ability))
+    }
+
+    "have no primary ability" in {
+      val card = new Ship("Test Card", new CardType("Ship"), None, None)
+      card.getPrimaryAbility should be(None)
+    }
+
+    "have an ally ability" in {
+      val allyAbility = new Ability(List("Gain 2 Trade"))
+      val card = new Ship("Test Card", new CardType("Ship"), None, None, Some(allyAbility))
+      card.getAllyAbility should be(Some(allyAbility))
+    }
+
+    "have no ally ability" in {
+      val card = new Ship("Test Card", new CardType("Ship"), None, None, None)
+      card.getAllyAbility should be(None)
+    }
+
+    "have a scrap ability" in {
+      val scrapAbility = new Ability(List("Scrap this card"))
+      val card = new Ship("Test Card", new CardType("Ship"), None, None, None, Some(scrapAbility))
+      card.getScrapAbility should be(Some(scrapAbility))
+    }
+
+    "have no scrap ability" in {
+      val card = new Ship("Test Card", new CardType("Ship"), None, None, None, None)
+      card.getScrapAbility should be(None)
+    }
+  }
+
+  "A Ship" should {
+
+    "be a ship" in {
+      val card = new Ship("Test Card", new CardType("Ship"))
+      card.isShip should be(true)
+    }
+
+    "not be a base" in {
+      val card = new Ship("Test Card", new CardType("Ship"))
+      card.isBase should be(false)
+    }
+  }
+
+  "A Base" should {
+
+    "be a base" in {
+      val card = new Base("Test Card", new CardType("Base"))
+      card.isBase should be(true)
+    }
+
+    "not be a ship" in {
+      val card = new Base("Test Card", new CardType("Base"))
+      card.isShip should be(false)
+    }
+    "be an outpost" in {
+      val card = new Base("Test Card", new CardType("Base"))
+      card.isOutPost should be(true)
+    }
+    "not be an outpost" in {
+      val card = new Base("Test Card", new CardType("Base"))
+      card.isOutPost should be(false)
+    }
+  }
+
+  "A CardType" should {
 
     "have a name" in {
-      card.getName should be("TestCard")
+      val cardType = new CardType("Ship")
+      cardType.getName should be("Ship")
     }
-    "have at least one ability" in {
-      card.getAbility.getActions should contain("TestAbility")
+
+    "render its name" in {
+      val cardType = new CardType("Ship")
+      cardType.render() should be("Ship")
     }
-    "have a main ability" in {
-      card.getAbility should be(ability)
+  }
+
+  "A Faction" should {
+
+    "have a name" in {
+      val faction = new TradeFederation
+      faction.getName should be("Trade Federation")
     }
-    "have a toString method" in {
-      card.toString should be("Card(name=TestCard, cardType=CardType, ability=Ability(actions=[]))")
+
+    "render its name" in {
+      val faction = new TradeFederation
+      faction.render() should be("Trade Federation")
     }
-    "be equal to another card with the same name" in {
-      val card1 = new TestCard("TestCard", new CardType("TestCardType"), new Ability(List()))
-      val card2 = new TestCard("TestCard", new CardType("TestCardType"), new Ability(List()))
-      card1 should be(card2)
+    "be equal to another faction with the same name" in {
+      val faction1 = new TradeFederation
+      val faction2 = new TradeFederation
+      faction1 should be(faction2)
     }
-    "not be equal to another card with a different name" in {
-      val card1 = new TestCard("TestCard1", new CardType("TestCardType"), new Ability(List()))
-      val card2 = new TestCard("TestCard2", new CardType("TestCardType"), new Ability(List()))
-      card1 should not be card2
+    "not be equal to another faction with a different name" in {
+      val faction1 = new TradeFederation
+      val faction2 = new StarEmpire
+      faction1 should not be faction2
     }
-    "not be equal to null" in {
-      card should not be null
+  }
+
+  "A TradeFederation" should {
+
+    "be a faction" in {
+      val faction = new TradeFederation
+      faction.getName should be("Trade Federation")
     }
-    
+
+    "render its name" in {
+      val faction = new TradeFederation
+      faction.render() should be("Trade Federation")
+    }
+  }
+
+  "A StarEmpire" should {
+
+    "be a faction" in {
+      val faction = new StarEmpire
+      faction.getName should be("Star Empire")
+    }
+
+    "render its name" in {
+      val faction = new StarEmpire
+      faction.render() should be("Star Empire")
+    }
+  }
+
+  "A Blob" should {
+
+    "be a faction" in {
+      val faction = new Blob
+      faction.getName should be("Blob")
+    }
+
+    "render its name" in {
+      val faction = new Blob
+      faction.render() should be("Blob")
+    }
+  }
+
+  "A MachineCult" should {
+
+    "be a faction" in {
+      val faction = new MachineCult
+      faction.getName should be("Machine Cult")
+    }
+
+    "render its name" in {
+      val faction = new MachineCult
+      faction.render() should be("Machine Cult")
+    }
+  }
+
+  "An Ability" should {
+    "have actions" in {
+      val ability = new Ability(List("Draw a card", "Gain 2 Trade"))
+      ability.actions should be(List("Draw a card", "Gain 2 Trade"))
+    }
+    "not have actions" in {
+      val ability = new Ability(List())
+      ability.actions should be(List())
+    }
+    "render when actions are present" in {
+      val ability = new Ability(List("Draw a card", "Gain 2 Trade"))
+      ability.render() should be("Draw a card, Gain 2 Trade")
+    }
+    "render when no actions are present" in {
+      val ability = new Ability(List())
+      ability.render() should be("No actions available")
+    }
 
   }
 }
