f7fb2391cc615b3623d270aecb968936eccf45a1
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-05-11 17:56:49 +0200

    Refactors card rendering to abstract interface
    Moves `render` method implementation from concrete classes to the `Card` trait, enforcing consistency and promoting extensibility.
    
    Simplifies class design by delegating rendering logic to subclasses.
    
    No functional changes introduced.
    
    Refactors card rendering to trait interface
    
    Moves `render` method to the `Card` trait, enforcing consistency and simplifying class design. Removes redundant rendering logic from subclasses.
    
    No functional changes introduced.

--- starrealms/src/main/scala/de/htwg/se/starrealms/model/CardBridge.scala
+++ starrealms/src/main/scala/de/htwg/se/starrealms/model/CardBridge.scala
@@ -9,37 +9,26 @@ trait Card {
     def primaryAbility: Option[Ability]
     def allyAbility: Option[Ability]
     def scrapAbility: Option[Ability]
-    def render(): String = {
-        s"Card: $cardName, Cost: ${cost.getOrElse("N/A")}, Defense: ${defense.getOrElse("N/A")}, " +
-        s"Primary Ability: ${primaryAbility.map(_.render()).getOrElse("N/A")}, " +
-        s"Ally Ability: ${allyAbility.map(_.render()).getOrElse("N/A")}, " +
-        s"Scrap Ability: ${scrapAbility.map(_.render()).getOrElse("N/A")} #traitCard"
-    }
+    def render(): String
 }
 trait CardType {
     def cardType: String
-    def render(): String
+    //def render(): String
 }
 
 class Ship extends CardType {
     override def cardType: String = "Ship"
-    override def render(): String = "Ship #cardType"
+    //override def render(): String = "Ship #cardType"
 }
 class Base extends CardType {
     override def cardType: String = "Base"
-    override def render(): String = "Base #cardType"
+   // override def render(): String = "Base #cardType"
     def isOutpost: Boolean
 
 }
 
 class FactionCard extends Card {
     def faction: Faction
-    override def render(): String = {
-        s"FactionCard: $cardName, Cost: ${cost.getOrElse("N/A")}, Defense: ${defense.getOrElse("N/A")}, " +
-        s"Primary Ability: ${primaryAbility.map(_.render()).getOrElse("N/A")}, " +
-        s"Ally Ability: ${allyAbility.map(_.render()).getOrElse("N/A")}, " +
-        s"Scrap Ability: ${scrapAbility.map(_.render()).getOrElse("N/A")} #factionCard"
-    }
 }
 
 
