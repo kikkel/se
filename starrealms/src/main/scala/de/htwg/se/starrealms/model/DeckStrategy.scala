dc028ae83d0d934bc4e6bf67ef3c546bf93762b6
koeseazra <azra.koese@gmx.de>
2025-05-16 10:49:05 +0200

    Deckstrategy commented

--- starrealms/src/main/scala/de/htwg/se/starrealms/model/DeckStrategy.scala
+++ starrealms/src/main/scala/de/htwg/se/starrealms/model/DeckStrategy.scala
@@ -1,11 +1,5 @@
 package de.htwg.se.starrealms.model
 
-trait DeckStrategy {
-  def buildDeck(deckBuilder: DeckBuilder): Deck
-}
-
-class StandardDeckStrategy extends DeckStrategy {
-  override def buildDeck(deckBuilder: DeckBuilder): Deck =
-    deckBuilder.buildFromCSV("src/main/resources/StandardDeck.csv")
+class DeckStrategy {
 
 }
