e07f0d054bc9f965a9afdf5c502106f71f7ec401
koeseazra <azra.koese@gmx.de>
2025-05-28 13:37:11 +0200

    added comman buttons. theyre not connected with logic yet, but ran out of time sorry hehe ^^

--- starrealms/src/main/scala/de/htwg/se/starrealms/view/GraphicUI.scala
+++ starrealms/src/main/scala/de/htwg/se/starrealms/view/GraphicUI.scala
@@ -4,58 +4,89 @@ import de.htwg.se.starrealms.controller.{Controller, CommandHandler}
 import de.htwg.util.Observer
 import scala.swing._
 import scala.swing.event._
+import javax.swing.BorderFactory
 
 class GraphicUI(controller: Controller, onExit: () => Unit) extends MainFrame with Reactor with Observer {
-  title = "Star Realms"
-  preferredSize = new Dimension(800, 600)
+    title = "Star Realms"
+    preferredSize = new Dimension(800, 600)
 
-  private val commandHandler = new CommandHandler(controller)
+    private val commandHandler = new CommandHandler(controller)
 
-  // UI Components
-  private val statusLabel = new Label("Welcome to Star Realms!")
-  private val inputField = new TextField(20)
-  private val outputArea = new TextArea(10, 40) {
-    editable = false
-    lineWrap = true
-    wordWrap = true
-  }
-
-  // Layout
-  contents = new BorderPanel {
-    layout(statusLabel) = BorderPanel.Position.North
-    layout(outputArea) = BorderPanel.Position.Center
-    layout(inputField) = BorderPanel.Position.South
-
-    listenTo(inputField)
-    reactions += {
-      case _: EditDone => processInput()
+    // UI Components
+    private val statusLabel = new Label("Welcome to Star Realms!")
+    private val inputField = new TextField(20)
+    private val outputArea = new TextArea(10, 40) {
+        editable = false
+        lineWrap = true
+        wordWrap = true
     }
+
+    val startButton = new Button("Start Game")
+    val replenishButton = new Button("replenish Trade Row")
+    val resetButton = new Button("reset Game")
+    val exitButton = new Button("Exit Game")
+
+    val controlPanel = new BoxPanel(Orientation.Horizontal) {
+    border = BorderFactory.createTitledBorder("Actions")
+    contents += inputField
+    contents += startButton
+    contents += replenishButton
+    contents += resetButton
+    contents += exitButton
+    contents += inputField
   }
 
-  // Observer registrieren
-  controller.addObserver(this)
+    // Layout
+    contents = new BorderPanel {
+        layout(statusLabel) = BorderPanel.Position.North
+        layout(outputArea) = BorderPanel.Position.Center
+        //layout(inputField) = BorderPanel.Position.South
+        layout(controlPanel) = BorderPanel.Position.South
 
-  def processInput(): Unit = {
-    val inputText = inputField.text.trim
-    if (inputText == "x") {
-      onExit()
-      close()
-    } else if (inputText.nonEmpty) {
-      outputArea.append(s"> $inputText\n")
-      outputArea.append(commandHandler.processCommand(inputText) + "\n")
-      inputField.text = ""
-      // Kein updateStatus mehr nötig, update kommt durch Observer!
+        listenTo(inputField.keys)
+        reactions += {
+        case KeyPressed(`inputField`, Key.Enter, _, _) => processInput()
+        }
     }
-  }
 
-  // Observer-Methode: Wird bei jedem Controller-Update aufgerufen
-  override def update: Unit = {
-    outputArea.append("\n" + controller.getState + "\n")
-  }
+    // Observer registrieren
+    controller.addObserver(this)
 
-  def run(): Unit = {
-    visible = true
-    outputArea.append("Welcome to Star Realms!\n")
-    outputArea.append(controller.getState + "\n")
-  }
+    def processInput(): Unit = {
+        val inputText = inputField.text.trim
+        inputField.text = ""
+        inputText match {
+            case "x" =>
+            onExit()
+            close()
+            case "s" | "t" | "r" | "e" | "z" | "y" | "show" =>
+            outputArea.append(s"> $inputText\n")
+            val result = commandHandler.processCommand(inputText)
+            outputArea.append(result + "\n")
+            case p if p.matches("""p \d+""") =>
+            outputArea.append(s"> $inputText\n")
+            val result = commandHandler.processCommand(inputText)
+            outputArea.append(result + "\n")
+            case b if b.matches("""b \d+""") =>
+            outputArea.append(s"> $inputText\n")
+            val result = commandHandler.processCommand(inputText)
+            outputArea.append(result + "\n")
+            case other if other.nonEmpty =>
+            outputArea.append(s"> $inputText\n")
+            outputArea.append("Ungültiger Befehl!\n")
+            case _ =>
+        }
+    }
+
+    // Observer-Methode: Wird bei jedem Controller-Update aufgerufen
+    override def update: Unit = {
+        outputArea.append("\n" + controller.getState + "\n")
+    }
+
+    def run(): Unit = {
+        visible = true
+        inputField.requestFocusInWindow()
+        outputArea.append("Welcome to Star Realms!\n")
+        outputArea.append(controller.getState + "\n")
+    }
 }
