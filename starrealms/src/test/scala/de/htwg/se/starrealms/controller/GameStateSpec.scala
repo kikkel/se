af96874c90302fd418e9213d24dfd1115ced23c2
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-05-30 11:25:05 +0200

    gamestate spec

--- starrealms/src/test/scala/de/htwg/se/starrealms/controller/GameStateSpec.scala
+++ starrealms/src/test/scala/de/htwg/se/starrealms/controller/GameStateSpec.scala
@@ -10,13 +10,19 @@ class GameStateSpec extends AnyWordSpec with Matchers {
 
   "A GameState" should {
     var updateCalled = false
+    val gameLogic = new GameLogic(new GameState(Map(), Player("Player1", 50), Player("Player2", 50)) ) {
+      override def notifyObservers(): Unit = { updateCalled = true }
+    }
+   
+ 
+
 
     "draw a card and notify observers" in {
       var drawCardCalled = false
       val observer = new Observer { override def update: Unit = { updateCalled = true } }
 
       val card = new DefaultCard(
-        set = Set("Core Set"),
+        edition = Edition("Core Set"),
         cardName = "Test Card",
         primaryAbility = None,
         faction = Faction("Unaligned"),
@@ -30,10 +36,8 @@ class GameStateSpec extends AnyWordSpec with Matchers {
           Some(card)
         }
       }
-      val gameState = new GameState { override val playerDeck: Deck = mdeck }
-      gameState.addObserver(observer)
 
-      val drawnCard = gameState.drawCard()
+      val drawnCard = gameLogic.drawCard()
 
       drawnCard should not be (None)
       assert(drawCardCalled, "drawCard should have been called")
@@ -48,10 +52,8 @@ class GameStateSpec extends AnyWordSpec with Matchers {
         }
       }
       val observer = new Observer { override def update: Unit = { updateCalled = true } }
-      val gameState = new GameState { override val playerDeck: Deck = mdeck }
-      gameState.addObserver(observer)
 
-      gameState.resetGame()
+      gameLogic.resetGame()
 
       assert(resetDeckCalled, "resetDeck should have been called")
       assert(updateCalled, "update should have been called")
@@ -59,7 +61,7 @@ class GameStateSpec extends AnyWordSpec with Matchers {
 
     "test playCard, buyCard, returnCardToPlayerDeck, returnCardToHand, undoReplenish, returnCardToTradeRow, endTurn, undoEndTurn, undoResetGame, getHand, getTradeRow, getDeckState" in {
       val card = new DefaultCard(
-        set = Set("Core Set"),
+        edition = Edition("Core Set"),
         cardName = "Test Card",
         primaryAbility = None,
         faction = Faction("Unaligned"),
@@ -67,72 +69,126 @@ class GameStateSpec extends AnyWordSpec with Matchers {
         qty = 1,
         role = "Trade Deck"
       )
-      val gameState = new GameState
+      val gameState = new GameState(
+        gameLogic.gameState.decksByRole,
+        Player("Player1", 50),
+        Player("Player2", 50)
+      )
+      val player1 = gameState.getCurrentPlayer
+
 
       // playCard
-      gameState.drawCards(1)
-      val handBefore = gameState.getHand.size
-      gameState.playCard(gameState.getHand.head)
-      gameState.getHand.size shouldBe (handBefore - 1)
+      gameLogic.drawCards(1)
+      val handBefore = gameLogic.gameState.getHand(player1).size
+      gameLogic.playCard(gameLogic.gameState.getHand(player1).head)
+      gameLogic.gameState.getHand(player1).size shouldBe (handBefore - 1)
 
       // buyCard
-      gameState.initTradeRow()
+      gameLogic.gameState.getTradeRow
       val tradeRowBefore = gameState.getTradeRow.size
       if (gameState.getTradeRow.nonEmpty) {
         val buyCard = gameState.getTradeRow.head
-        gameState.buyCard(buyCard)
+        gameLogic.buyCard(buyCard)
         gameState.getTradeRow.size should be <= tradeRowBefore
       }
 
       // returnCardToPlayerDeck
-      gameState.drawCards(1)
-      if (gameState.getHand.nonEmpty) {
-        val cardToReturn = gameState.getHand.head
-        gameState.returnCardToPlayerDeck(cardToReturn)
-        gameState.getHand should not contain cardToReturn
+      gameLogic.drawCards(1)
+      if (gameLogic.gameState.getHand(player1).nonEmpty) {
+        val cardToReturn = gameState.getHand(player1).head
+        gameLogic.returnCardToPlayerDeck(cardToReturn)
+        gameState.getHand(player1) should not contain cardToReturn
       }
 
       // returnCardToHand
-      gameState.drawCards(1)
-      if (gameState.getHand.nonEmpty) {
-        val cardToReturn = gameState.getHand.head
-        gameState.playCard(cardToReturn)
-        gameState.returnCardToHand(cardToReturn)
-        gameState.getHand should contain(cardToReturn)
+      gameLogic.drawCards(1)
+      if (gameState.getHand(player1).nonEmpty) {
+        val cardToReturn = gameState.getHand(player1).head
+        gameLogic.playCard(cardToReturn)
+        gameLogic.returnCardToHand(cardToReturn)
+        gameState.getHand(player1) should contain(cardToReturn)
       }
 
       // undoReplenish
-      gameState.initTradeRow()
+      gameLogic.replenishTradeRow()
       if (gameState.getTradeRow.nonEmpty) {
         val cardToUndo = gameState.getTradeRow.head
-        gameState.undoReplenish(cardToUndo)
+        gameLogic.undoReplenish(cardToUndo)
         gameState.getTradeRow should not contain cardToUndo
-        gameState.tradeDeck.getCards should contain(cardToUndo)
+        gameState.getTradeDeck.getCards should contain(cardToUndo)
 
       }
 
       // returnCardToTradeRow
-      gameState.initTradeRow()
+      gameLogic.replenishTradeRow()
       if (gameState.getTradeRow.nonEmpty) {
         val cardToReturn = gameState.getTradeRow.head
-        gameState.buyCard(cardToReturn)
-        gameState.returnCardToTradeRow(cardToReturn)
+        gameLogic.buyCard(cardToReturn)
+        gameLogic.returnCardToTradeRow(cardToReturn)
         gameState.getTradeRow should contain(cardToReturn)
       }
 
       // endTurn und undoEndTurn
-      gameState.drawCards(5)
-      gameState.endTurn()
-      gameState.undoEndTurn()
+      gameLogic.drawCards(5)
+      gameLogic.endTurn()
+      gameLogic.undoEndTurn()
 
       // undoResetGame
-      gameState.undoResetGame()
+      gameLogic.undoResetGame()
 
       // getHand, getTradeRow, getDeckState
-      gameState.getHand shouldBe a [List[_]]
+      //gameState.getHand(player1). shouldBe a [List[_]]
       gameState.getTradeRow shouldBe a [List[_]]
       gameState.getDeckState shouldBe a [String]
     }
     
+    "allow access to basic getters and state updates" in {
+      val p1 = Player("Player1", health = 50)
+      val p2 = Player("Player2", health = 50)
+      val decks = Map("Personal Deck" -> new Deck(), "Trade Deck" -> new Deck(), "Explorer Pile" -> new Deck())
+      val gameState = new GameState(decks, p1, p2)
+
+      gameState.getCurrentPlayer shouldBe p1
+      gameState.getOpponent shouldBe p2
+
+      gameState.setCurrentPlayer(p2)
+      gameState.setOpponent(p1)
+      gameState.getCurrentPlayer shouldBe p2
+      gameState.getOpponent shouldBe p1
+
+      val newDeck = new Deck()
+      gameState.setPlayerDeck(p1, newDeck)
+      gameState.getPlayerDeck(p1) shouldBe newDeck
+
+      gameState.getDiscardPile(p1) shouldBe empty
+      gameState.getDiscardPiles should contain key p1
+
+      gameState.setLastDiscardedHand(p1, List())
+      gameState.getLastDiscardedHand(p1) shouldBe empty
+
+      val tradeDeck = new Deck()
+      val explorerDeck = new Deck()
+      gameState.setTradeDeck(tradeDeck)
+      gameState.setExplorerPile(explorerDeck)
+      gameState.getTradeDeck shouldBe tradeDeck
+      gameState.getExplorerPile shouldBe explorerDeck
+
+      noException should be thrownBy gameState.notifyStateChange()
+    }
+
+    "check game over conditions correctly" in {
+      val p1dead = Player("P1", health = 0)
+      val p2alive = Player("P2", health = 50)
+      val gameOverState = new GameState(Map(), p1dead, p2alive)
+      gameOverState.checkGameOver() shouldBe Some("P2 won!")
+
+      val p1alive = Player("P1", health = 50)
+      val p2dead = Player("P2", health = 0)
+      val gameOverState2 = new GameState(Map(), p1alive, p2dead)
+      gameOverState2.checkGameOver() shouldBe Some("P1 won!")
+
+      val drawState = new GameState(Map(), p1alive, p2alive)
+      drawState.checkGameOver() shouldBe None
+    }
   }
 }
