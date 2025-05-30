accbfe5f9b6fcfb1b34fe87c3051f8e6fbee75a7
koeseazra <azra.koese@gmx.de>
2025-05-30 11:19:22 +0200

    CommandSpec fixed errors

--- starrealms/src/test/scala/de/htwg/se/starrealms/controller/CommandSpec.scala
+++ starrealms/src/test/scala/de/htwg/se/starrealms/controller/CommandSpec.scala
@@ -8,51 +8,70 @@ import scala.util.{Try, Success, Failure}
 
 class CommandSpec extends AnyWordSpec with Matchers {
 
+  def dummyGameLogic: GameLogic = {
+    val edition = Edition("Core Set")
+    val player1 = Player("Player 1")
+    val player2 = Player("Player 2")
+    val decksByRole = Map("Personal Deck" -> new Deck(), "Trade Deck" -> new Deck(), "Trade Row" -> new Deck())
+    val gameState = new GameState(decksByRole, player1, player2)
+    new GameLogic(gameState)
+  }
+  def dummyController: Controller = new Controller(dummyGameLogic)
+
+  def dummyDefaultCard(name: String = "Scout") = new DefaultCard(
+    Edition("Core Set"),
+    name,
+    Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
+    Faction("Unaligned"),
+    Success(new Ship()),
+    1,
+    "Personal Deck"
+  )
+
+  def dummyFactionCard(name: String = "Card A", cost: Int = 1) = new FactionCard(
+    edition = Edition("Core Set"),
+    cardName = name,
+    cost = cost,
+    primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
+    allyAbility = None,
+    scrapAbility = None,
+    faction = Faction("Unaligned"),
+    cardType = Success(new Ship()),
+    qty = 1,
+    role = "Trade Row",
+    notes = None
+  )
+
   "A DrawCardCommand" should {
-     "store the drawn card on doMove"  in {
-      val controller = new Controller()
-      val card = new DefaultCard(
-        set = Set("Core Set"),
-        cardName = "Viper",
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Personal Deck"
-      )
-      controller.gameState.getHand.foreach(controller.gameState.playCard)
-      controller.gameState.returnCardToPlayerDeck(card)
+    "store the drawn card on doMove"  in {
+      val controller = dummyController
+      val card = dummyDefaultCard("Viper")
+      controller.gameState.setHand(controller.gameState.getCurrentPlayer, Nil)
+      controller.gameState.getPlayerDeck(controller.gameState.getCurrentPlayer).addCard(card)
       val command = new DrawCardCommand(controller)
       command.doMove
+      controller.gameState.getHand(controller.gameState.getCurrentPlayer) should contain(card)
     }
     "return the drawn card on undoMove and clear it" in {
-      val controller = new Controller()
-      val card = new DefaultCard(
-        set = Set("Core Set"),
-        cardName = "Viper",
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Personal Deck"
-      )
-      controller.gameState.getHand.foreach(controller.gameState.playCard)
-      controller.gameState.returnCardToPlayerDeck(card)
+      val controller = dummyController
+      val card = dummyDefaultCard("Viper")
+      controller.gameState.setHand(controller.gameState.getCurrentPlayer, Nil)
+      controller.gameState.getPlayerDeck(controller.gameState.getCurrentPlayer).addCard(card)
       val command = new DrawCardCommand(controller)
       command.doMove
       command.undoMove
+      controller.gameState.getHand(controller.gameState.getCurrentPlayer) should not contain card
     }
     "not fail when undoMove is called without a drawn card" in {
-      val controller = new Controller()
+      val controller = dummyController
       val command = new DrawCardCommand(controller)
-      command.undoMove // should be a no-op
-
+      noException should be thrownBy command.undoMove
     }
   }
 
   "A ResetGameCommand" should {
     "reset the game and allow undo" in {
-      val controller = new Controller()
+      val controller = dummyController
       val command = new ResetGameCommand(controller)
       noException should be thrownBy command.doMove
       noException should be thrownBy command.undoMove
@@ -61,7 +80,7 @@ class CommandSpec extends AnyWordSpec with Matchers {
 
   "A ShowDeckCommand" should {
     "print deck state" in {
-      val controller = new Controller()
+      val controller = dummyController
       val command = new ShowDeckCommand(controller)
       noException should be thrownBy command.doMove
       command.undoMove // should do nothing
@@ -78,44 +97,22 @@ class CommandSpec extends AnyWordSpec with Matchers {
 
   "A PlayCardCommand" should {
     "play and undo a card" in {
-      val controller = new Controller()
-      val card = new DefaultCard(
-        set = Set("Core Set"),
-        cardName = "Scout",
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Personal Deck"
-      )
-      // Hand leeren
-      controller.gameState.getHand.foreach(controller.gameState.playCard)
-      // Karte auf die Hand legen
-      controller.gameState.returnCardToHand(card)
+      val controller = dummyController
+      val card = dummyDefaultCard("Scout")
+      controller.gameState.setHand(controller.gameState.getCurrentPlayer, List(card))
       val command = new PlayCardCommand(controller, card)
       command.doMove
-      controller.gameState.getHand should not contain card
+      controller.gameState.getHand(controller.gameState.getCurrentPlayer) should not contain card
       command.undoMove
-      controller.gameState.getHand should contain(card)
+      controller.gameState.getHand(controller.gameState.getCurrentPlayer) should contain(card)
     }
   }
 
   "A BuyCardCommand" should {
     "buy and undo a card" in {
-      val controller = new Controller()
-      val card = new DefaultCard(
-        set = Set("Core Set"),
-        cardName = "Scout",
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Personal Deck"
-      )
-      // TradeRow leeren
-      controller.gameState.getTradeRow.foreach(controller.gameState.buyCard)
-      // Karte in die TradeRow legen
-      controller.gameState.returnCardToTradeRow(card)
+      val controller = dummyController
+      val card = dummyDefaultCard("Scout")
+      controller.gameState.setTradeRow(List(card))
       val command = new BuyCardCommand(controller, card)
       command.doMove
       controller.gameState.getTradeRow should not contain card
@@ -126,9 +123,8 @@ class CommandSpec extends AnyWordSpec with Matchers {
 
   "An EndTurnCommand" should {
     "end and undo turn" in {
-      val controller = new Controller()
-      // Hand leeren
-      controller.gameState.getHand.foreach(controller.gameState.playCard)
+      val controller = dummyController
+      controller.gameState.setHand(controller.gameState.getCurrentPlayer, Nil)
       val command = new EndTurnCommand(controller)
       noException should be thrownBy command.doMove
       noException should be thrownBy command.undoMove
@@ -137,124 +133,58 @@ class CommandSpec extends AnyWordSpec with Matchers {
 
   "A DrawCardsCommand" should {
     "draw multiple cards and undo" in {
-      val controller = new Controller()
-      val card = new DefaultCard(
-        set = Set("Core Set"),
-        cardName = "Scout",
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Personal Deck"
-      )
-      // Hand leeren
-      controller.gameState.getHand.foreach(controller.gameState.playCard)
-      // Deck mit Karten füllen
-      (1 to 3).foreach(_ => controller.gameState.returnCardToPlayerDeck(card))
+      val controller = dummyController
+      val card = dummyDefaultCard("Scout")
+      controller.gameState.setHand(controller.gameState.getCurrentPlayer, Nil)
+      (1 to 3).foreach(_ => controller.gameState.getPlayerDeck(controller.gameState.getCurrentPlayer).addCard(card))
       val command = new DrawCardsCommand(controller, 2)
       command.doMove
-      controller.gameState.getHand.size shouldBe 2
+      controller.gameState.getHand(controller.gameState.getCurrentPlayer).size shouldBe 2
       command.undoMove
-      controller.gameState.getHand.size shouldBe 0
+      controller.gameState.getHand(controller.gameState.getCurrentPlayer).size shouldBe 0
     }
   }
 
   "A ReplenishTradeRowCommand" should {
     "call replenishTradeRow on doMove" in {
-      val controller = new Controller()
+      val controller = dummyController
       val command = new ReplenishTradeRowCommand(controller)
-      val card = new FactionCard(
-        set = Set("Core Set"),
-        cardName = "Card A",
-        cost = 1,
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
-        allyAbility = None,
-        scrapAbility = None,
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Trade Row"
-      )
-
       command.doMove
-
+      // Keine Exception = Test bestanden
     }
     "call undoReplenish for each card on undoMove" in {
-      val controller = new Controller()
+      val controller = dummyController
       val command = new ReplenishTradeRowCommand(controller)
-      val card1 = new FactionCard(
-        set = Set("Core Set"),
-        cardName = "Card A",
-        cost = 1,
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
-        allyAbility = None,
-        scrapAbility = None,
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Trade Row"
-      )
-      val card2 = new FactionCard(
-        set = Set("Core Set"),
-        cardName = "Card B",
-        cost = 2,
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 2 Trade")))),
-        allyAbility = None,
-        scrapAbility = None,
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Trade Row"
-      )
-
+      val card1 = dummyFactionCard("Card A", 1)
+      val card2 = dummyFactionCard("Card B", 2)
       command.setRep(List(card1, card2))
-      command.undoMove
+      noException should be thrownBy command.undoMove
     }
-
     "call replenishTradeRow on redoMove" in {
-      val controller = new Controller()
+      val controller = dummyController
       val command = new ReplenishTradeRowCommand(controller)
-      val card = new FactionCard(
-        set = Set("Core Set"),
-        cardName = "Card A",
-        cost = 1,
-        primaryAbility = Some(new Ability(List(SimpleAction("Gain 1 Trade")))),
-        allyAbility = None,
-        scrapAbility = None,
-        faction = Faction("Unaligned"),
-        cardType = Success(new Ship()),
-        qty = 1,
-        role = "Trade Row"
-      )
-
       command.doMove
-      command.redoMove
+      noException should be thrownBy command.redoMove
     }
   }
-"undoMove" should {
 
+  "undoMove" should {
     "return a message when there are no moves to undo" in {
       val manager = new UndoManager
       manager.getUndoStack shouldBe empty
-
       val result = manager.undoMove
-
       result shouldBe "No moves to undo #Command"
       manager.getRedoStack shouldBe empty
     }
   }
 
   "redoMove" should {
-
     "return a message when there are no moves to redo" in {
       val manager = new UndoManager
       manager.getRedoStack shouldBe empty
-
       val result = manager.redoMove
-
       result shouldBe "No moves to redo #Command"
       manager.getUndoStack shouldBe empty
     }
   }
-
 }
