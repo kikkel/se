0615f8a4e900a31134d7042799ce6efc07a32f6f
koeseazra <azra.koese@gmx.de>
2025-05-30 11:18:12 +0200

    FactorySpec fixed

--- starrealms/src/test/scala/de/htwg/se/starrealms/model/FactorySpec.scala
+++ starrealms/src/test/scala/de/htwg/se/starrealms/model/FactorySpec.scala
@@ -16,14 +16,24 @@ class FactorySpec extends AnyWordSpec with Matchers {
     "throw an exception for unknown faction names" in {
       an [IllegalArgumentException] should be thrownBy { Faction("pirates") }
     }
+
+    "handle composite factions" in {
+      val composite = Faction("star empire / blob")
+      composite.factionName shouldBe "Star Empire / Blob"
+      composite.matches(Faction("star empire")) shouldBe true
+      composite.matches(Faction("blob")) shouldBe true
+      composite.matches(Faction("machine cult")) shouldBe false
+    }
   }
 
-  "Set Abstract Factory" should {
-    "return correct instances for known set names" in {
-      Set("core set").nameOfSet shouldBe "Core Set"
+  "Edition Abstract Factory" should {
+    "return correct instances for known edition names" in {
+      Edition("core set").nameOfEdition shouldBe "Core Set"
+      Edition("colony wars").nameOfEdition shouldBe "Colony Wars"
+      Edition("frontiers").nameOfEdition shouldBe "Frontiers"
     }
-    "throw an exception for unknown set names" in {
-      an [IllegalArgumentException] should be thrownBy { Set("pirates set") }
+    "throw an exception for unknown edition names" in {
+      an [IllegalArgumentException] should be thrownBy { Edition("pirates set") }
     }
   }
 }
