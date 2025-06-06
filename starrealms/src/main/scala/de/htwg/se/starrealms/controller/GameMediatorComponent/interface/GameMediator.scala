d654b184b5ebfba06bb0283905bc0eae45387686
Kiani Moon Wellens <kianiwellens@gmail.com>
2025-06-06 10:39:39 +0200

    getter for gamemediator

--- starrealms/src/main/scala/de/htwg/se/starrealms/controller/GameMediatorComponent/interface/GameMediator.scala
+++ starrealms/src/main/scala/de/htwg/se/starrealms/controller/GameMediatorComponent/interface/GameMediator.scala
@@ -1,5 +1,11 @@
 package de.htwg.se.starrealms.controller.GameMediatorComponent.interface
 
+import de.htwg.se.starrealms.controller.GameLogicComponent.interface.GameLogicInterface
+import de.htwg.se.starrealms.model.GameStateComponent.interface.GameStateInterface
+import de.htwg.se.starrealms.model.PlayerComponent.interface.PlayerInterface
 trait GameMediator {
     def notify(sender: Any, event: String): Unit
+    def getGameLogic: GameLogicInterface
+    def getGameState: GameStateInterface
+    def getCurrentPlayer: PlayerInterface
 }
